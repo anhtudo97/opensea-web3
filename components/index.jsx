fe425a34ebef1f822e495042f754fe795b0e54df
anhtudo97 <anhtudo97@gamil.com>
2022-08-02 23:42:52 +0700

    feat: add component

---
+++ components/index.jsx
@@ -0,0 +1,5 @@
+import Header from "./Header";
+import Logo from "./Logo";
+import SearchInput from "./SearchInput";
+
+export { Header, Logo, SearchInput };
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider
+      desiredChainId={ChainId.Rinkeby}
+      chainRpc={{
+        [ChainId.Rinkeby]:
+          "https://rinkeby.infura.io/v3/99e321df6b7641e695169ee4d5b10f00",
+      }}
+    >
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
